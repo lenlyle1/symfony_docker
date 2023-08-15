15e740ec7e42ba8bfc900cdb775727f38dc55157
lenlyle@gmail.com <lenye@gmail.com>
2023-08-15 12:53:22 +0100

    adding docker setup

---
+++ codebase/tests/Unit/RegisterTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class RegisterTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
