7e7f3f8aee091515ebee831252abb05653fab9b8
lenlyle@gmail.com <lenye@gmail.com>
2023-08-15 13:12:40 +0100

    Added phpunit

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '15e740ec7e42ba8bfc900cdb775727f38dc55157',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '15e740ec7e42ba8bfc900cdb775727f38dc55157',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
